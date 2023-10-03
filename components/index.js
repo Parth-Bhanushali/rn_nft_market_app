f99e7efa37c90a09a66d1bb3ab3b90c6d53715ef
Parth Bhanushali <princebhanushali6@gmail.com>
2023-10-04 01:01:17 +0530

    building Home screen UI with FlatList

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import HomeHeader from  './HomeHeader'
+import NFTCard from './NFTCard'
+import FocusedStatusBar from './FocusedStatusBar'
+import {CircleButton, RectangularButton} from './Button'
+
+export {
+    HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectangularButton
+}
