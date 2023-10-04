ad1a2348589611486653093f44b7c8f888d57f57
Parth Bhanushali <princebhanushali6@gmail.com>
2023-10-04 12:02:21 +0530

    building UI for Details screen

--- components/index.js
+++ components/index.js
@@ -2,7 +2,11 @@ import HomeHeader from  './HomeHeader'
 import NFTCard from './NFTCard'
 import FocusedStatusBar from './FocusedStatusBar'
 import {CircleButton, RectangularButton} from './Button'
+import DetailsBid from './DetailsBid'
+import DetailsDescription from './DetailsDescription'
+import {SubInfo} from './SubInfo'
 
 export {
-    HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectangularButton
+    HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectangularButton, DetailsDescription, DetailsBid,
+    SubInfo
 }
